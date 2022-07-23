493988ba6de5b9bc231c081ea4fa3287bda6b618
uc18 <>
2022-07-23 14:03:38 +0400

    fix auth error

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -37,21 +37,20 @@ def _get_authorization_header_retriever(
 def _get_authorization_header(
     api_key: str = Security(RWAPIKeyHeader(name=HEADER_KEY)),
 ) -> str:
-    #try:
-        #token_prefix, token = api_key.split(" ")
-    token = api_key
-    #except ValueError:
-    #    raise HTTPException(
-    #        status_code=status.HTTP_403_FORBIDDEN,
-    #        detail={"msg": "WRONG_TOKEN_PREFIX"},
-    #    )
-    #if token_prefix != "Bearer":
-    #    raise HTTPException(
-    #        status_code=status.HTTP_403_FORBIDDEN,
-    #        detail={"msg": "WRONG_TOKEN_PREFIX"},
-    #    )
-
-    return token
+        try:
+            token_prefix, token = api_key.split(" ")
+            token = api_key
+        except ValueError:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={"msg": "WRONG_TOKEN_PREFIX"},
+            )
+        if token_prefix != "Bearer":
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={"msg": "WRONG_TOKEN_PREFIX"},
+            )
+        return token
 
 
 def _get_authorization_header_optional(
